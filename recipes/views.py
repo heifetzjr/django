807de0d584842496360e1b505d88e510183035db
Antonio Bernardo Dos Santos Junior <bernardojunior.ccb@gmail.com>
2026-03-17 18:43:57 -0300

    atualizando pastas

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
 
-# Create your views here.
+# Create your views here
 def home(request: HttpRequest):
     context = {
         'own': "Bernardo"
